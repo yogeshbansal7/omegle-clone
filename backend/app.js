d226ed293bdc1b258d72f896d31ba3f9988ae410
yogesbansal7 <ybansal961@gmail.com>
2024-08-08 10:06:05 +0530

    calling dummy api so that server never shutsdown

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ const cors = require("cors")
 const { createServer } = require("http")
 const socketSetup = require("./socket"); 
 require("dotenv").config()
+const http = require('http');
+
 
 const app = express()
 
@@ -20,7 +22,38 @@ app.get("/", (req, res) => {
     res.send("Hello this is test message")
 })
 
+app.get("/ping", (req, res) => {
+    res.send("pong");
+    return;
+});
+
 const port = process.env.PORT || 5000
+
+
+function pingServer() {
+    const options = {
+      hostname: 'localhost',
+      port: process.env.PORT || 5000,
+      path: '/ping',
+      method: 'GET'
+    };
+  
+    const req = http.request(options, (res) => {
+      res.on('data', (chunk) => {
+        // console.log(`Ping response: ${chunk}`);
+        console.log("Calling dummy api")
+      });
+    });
+  
+    req.on('error', (e) => {
+      console.error(`Problem with ping request: ${e}`);
+    });
+  
+    req.end();
+  }
+  
+  setInterval(pingServer, 1000); 
+  
 
 server.listen(port, () => {                                                                                         
     console.log(`Server run on port http://localhost:${port}`)
